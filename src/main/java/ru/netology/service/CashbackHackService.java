0ff88ba6cc066c46712506db1da2c1ecb10b5766
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-09-04 14:57:44 +0300

    Added metods in CashbackHackService

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -3,7 +3,14 @@ package ru.netology.service;
 public class CashbackHackService {
     private final int boundary = 1000;
 
-    public int remain(int amount) {
-        return boundary - amount % boundary;
-    }
+       public int remain(int amount) {
+           int difference = boundary - amount % boundary;
+
+           if (difference == 0) {
+               System.out.println("Сделайте покупку на " + difference + " руб");
+               return difference;
+           }
+           return difference;
+       }
+
 }
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,37 @@
 package ru.netology.service;
-
-//import org.junit.jupiter.api.Test;
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test void shouldReturn() {
+    @Test
+    void shouldReturn100() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 1500;
+        int amount = 900;
 
-        int actual = 500;
+        int actual = 100;
+        int expected = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldReturn1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = 1000;
+        int expected = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldReturn1500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1600;
+
+        int actual = 400;
         int expected = service.remain(amount);
 
         assertEquals(actual, expected);
