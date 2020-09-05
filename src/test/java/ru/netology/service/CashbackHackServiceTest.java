284ddffc76e279dcdd994b080926ae8c2e4b92b6
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-09-05 10:39:06 +0300

    Fixed bug

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = 100;
+        int expected = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldReturn1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = 1000;
+        int expected = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldReturn1500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1600;
+
+        int actual = 400;
+        int expected = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+}
