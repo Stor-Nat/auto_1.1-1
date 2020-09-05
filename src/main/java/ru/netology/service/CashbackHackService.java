3c4c0b5a95d3ebdc8a6c1dd1d2d4c3079eac091b
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-09-05 19:51:32 +0300

    Deleted test

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -5,6 +5,12 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
+        int difference = boundary - amount % boundary;
+
+        if (amount % 1000 != 0) {
+            System.out.println("Сделайте покупку на " + difference + " руб");
+            return difference;
+        }
+        return difference;
     }
 }
